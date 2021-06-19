c0dfe53964e4545fa207a306aaf8e14355a85b2e
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-06-19 15:02:11 -0300

    Update config

--- src/Config/config.js
+++ src/Config/config.js
@@ -3,8 +3,22 @@ import 'phaser';
 export default {
   width: 800,
   height: 600,
+  backgroundColor: "black",
   parent: 'phaser',
-  type: Phaser.AUTO,
+  type: Phaser.WEBGL,
   autoCenter: 1,
-  scaleMode: 3
+  scaleMode: 3,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  scene: [
+    // SceneMainMenu,
+    // SceneMain,
+    // SceneGameOver
+  ],
+  pixelArt: true,
+  roundPixels: true
 };
