5a02239b690cc632b40d1e7670dbadd675802ff0
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-06-19 17:37:12 -0300

    Add re-enable golds and silvers when collect all

--- src/Scenes/SceneMain.js
+++ src/Scenes/SceneMain.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser'
-import { Player } from '../Entities'
+import {
+  Player
+} from '../Entities'
+
 export default class SceneMain extends Phaser.Scene {
   constructor() {
     super('SceneMain');
@@ -10,83 +13,50 @@ export default class SceneMain extends Phaser.Scene {
     this.load.image('things_silver', '../../src/assets/visual/things_silver.png');
     this.load.image('enemyRed1', '../../src/assets/visual/enemyRed1.png');
     this.load.image('enemyRed3', '../../src/assets/visual/enemyRed3.png');
-    
-    this.load.image('sprBg1', '../../src/assets/visual/sprBg1.png');
+
     this.load.spritesheet('sprExplosion', '../../src/assets/visual/sprExplosion.png', {
       frameWidth: 80,
       frameHeight: 80
     });
-   
-  
-this.load.spritesheet('sprEnemy0', '../../src/assets/visual/sprEnemy0.png', {
-      frameWidth: 16,
-      frameHeight: 16
-    });
-
-    this.anims.create({
-      key: 'enemy0',
-      frames: this.anims.generateFrameNumbers('sprEnemy0', { start: 0, end: 2 }),
-      frameRate: 20,
-      repeat: -1
-    });
 
 
-    this.load.image('sprLaserEnemy0', '../../src/assets/visual/sprLaserEnemy0.png');
-    this.load.image('sprLaserPlayer', '../../src/assets/visual/sprLaserPlayer.png');
     this.load.spritesheet('sprPlayer', '../../src/assets/visual/sprPlayer.png', {
       frameWidth: 16,
       frameHeight: 16
     });
-    this.load.audio('sndExplode0', ['../../src/assets/TownTheme.mp3']);
-    //this.load.audio('sndExplode0', ['../../src/assets/sound/sndExplode0.wav']);
-    this.load.audio('sndExplode1', '../../src/assets/sound/sndExplode1.wav');
-    this.load.audio('sndLaser', '../../src/assets/sound/sndLaser.wav');
 
-    
-
-
-    this.anims.create({
-      key: 'sprEnemy2',
-      frames: this.anims.generateFrameNumbers('sprEnemy2', { start: 0, end: 3 }),
-      frameRate: 20,
-      repeat: -1
-    });
 
     this.anims.create({
       key: 'sprExplosion',
-      frames: this.anims.generateFrameNumbers('sprExplosion', { start: 2, end: 2 }),
+      frames: this.anims.generateFrameNumbers('sprExplosion', {
+        start: 2,
+        end: 2
+      }),
       frameRate: 20,
       repeat: -1
     });
 
     this.anims.create({
       key: 'sprPlayer',
-      frames: this.anims.generateFrameNumbers('sprPlayer', { start: 0, end: 3 }),
+      frames: this.anims.generateFrameNumbers('sprPlayer', {
+        start: 0,
+        end: 3
+      }),
       frameRate: 20,
       repeat: -1
     });
 
-
-    // this.sfx = {
-    //   explosions: [
-    //     this.sound.add('sndExplode0'),
-    //     this.sound.add('sndExplode1')
-    //   ],
-    //   laser: this.sound.add('sndLaser')
-    // };
-
-
-
- 
-
-
-
-
-
   }
 
-  
+
   create() {
+
+    scoreText = this.add.text(660, 16, 'Score: 0', {
+      fontSize: '20px',
+      fill: '#ff0'
+    });
+
+
     this.player = new Player(
       this,
       this.game.config.width * 0.5,
@@ -94,67 +64,135 @@ this.load.spritesheet('sprEnemy0', '../../src/assets/visual/sprEnemy0.png', {
       'sprPlayer'
     );
 
-    const golds = this.physics.add.group({
-    key: 'things_gold',
-    setScale: { x: 0.7, y: 0.7},
-    repeat: 9,
-    setXY: { x: 40, y: 0, stepX: 70 },
-    bounceY: 1,
-    bounceX: 1,
-    collideWorldBounds: true
-});
+    this.golds = this.physics.add.group({
+      key: 'things_gold',
+      setScale: {
+        x: 0.7,
+        y: 0.7
+      },
+      repeat: 7,
+      setXY: {
+        x: 40,
+        y: 0,
+        stepX: 70
+      },
+      bounceY: 1,
+      bounceX: 1,
+      collideWorldBounds: true
+    });
 
-    golds.setVelocityY(10, 40);
-    golds.setVelocityX(5, 15);
-    this.physics.add.overlap(this.player, golds, goldCollected, null, this);
-
-    const silvers = this.physics.add.group({
-    key: 'things_silver',
-    setScale: { x: 0.7, y: 0.7},
-    repeat: 9,
-    setXY: { x: 40, y: 30, stepX: 70 },
-    bounceY: 1,
-    bounceX: 1,
-    collideWorldBounds: true
-});
-
-    silvers.setVelocityY(5, 20);
-    silvers.setVelocityX(-5, -15);
-    this.physics.add.overlap(this.player, silvers, silverCollected, null, this);
+    this.golds.setVelocityY(10, 40);
+    this.golds.setVelocityX(5, 15);
 
 
- const enemies1 = this.physics.add.group({
-    key: 'enemyRed1',
-    setScale: { x: 0.5, y: 0.5},
-    repeat: 4,
-    setXY: { x: 40, y: 30, stepX: 150 },
-    bounceY: 1,
-    bounceX: 1,
-    collideWorldBounds: true
-});
+    const goldCollected = (player, gold) => {
+      gold.disableBody(true, true);
+      score += 10;
+      scoreText.setText('Score: ' + score);
+      console.log('Score: ', score)
+      if (this.golds.countActive(true) === 0) {
+        console.log('no more silvers')
+        this.golds.children.iterate(function (child) {
+
+          child.enableBody(true, Phaser.Math.Between(100, 800), Phaser.Math.Between(100, 600), true, true);
+
+
+        });
+        this.golds.setVelocityY(5, 20);
+        this.golds.setVelocityX(-5, -15);
+      }
+    }
+
+
+    this.silvers = this.physics.add.group({
+      key: 'things_silver',
+      setScale: {
+        x: 0.7,
+        y: 0.7
+      },
+      repeat: 7,
+      setXY: {
+        x: 40,
+        y: 30,
+        stepX: 70
+      },
+      bounceY: 1,
+      bounceX: 1,
+      collideWorldBounds: true
+    });
+
+    this.silvers.setVelocityY(5, 20);
+    this.silvers.setVelocityX(-5, -15);
+
+    const silverCollected = (player, silver) => {
+      silver.disableBody(true, true);
+      score += 5;
+      scoreText.setText('Score: ' + score);
+      console.log('Score: ', score)
+      if (this.silvers.countActive(true) === 0) {
+        console.log('no more silvers')
+        this.silvers.children.iterate(function (child) {
+
+          child.enableBody(true, Phaser.Math.Between(100, 800), Phaser.Math.Between(100, 600), true, true);
+
+
+        });
+        this.silvers.setVelocityY(5, 20);
+        this.silvers.setVelocityX(-5, -15);
+      }
+
+    }
+
+    this.physics.add.overlap(this.player, this.golds, goldCollected, null, this);
+    this.physics.add.overlap(this.player, this.silvers, silverCollected, null, this);
+
+    const enemies1 = this.physics.add.group({
+      key: 'enemyRed1',
+      setScale: {
+        x: 0.5,
+        y: 0.5
+      },
+      repeat: 4,
+      setXY: {
+        x: 40,
+        y: 30,
+        stepX: 150
+      },
+      bounceY: 1,
+      bounceX: 1,
+      collideWorldBounds: true
+    });
 
     enemies1.setVelocityY(5, 20);
     enemies1.setVelocityX(5, 15);
     this.physics.add.overlap(this.player, enemies1, enemiesCrashed, null, this);
 
-const enemies2 = this.physics.add.group({
-    key: 'enemyRed3',
-    setScale: { x: 0.5, y: 0.5},
-    repeat: 3,
-    setXY: { x: 100, y: 30, stepX: 120 },
-    bounceY: 1,
-    bounceX: 1,
-    collideWorldBounds: true
-});
+    const enemies2 = this.physics.add.group({
+      key: 'enemyRed3',
+      setScale: {
+        x: 0.5,
+        y: 0.5
+      },
+      repeat: 3,
+      setXY: {
+        x: 100,
+        y: 30,
+        stepX: 120
+      },
+      bounceY: 1,
+      bounceX: 1,
+      collideWorldBounds: true
+    });
 
     enemies2.setVelocityY(5, 20);
     enemies2.setVelocityX(-5, -15);
     this.physics.add.overlap(this.player, enemies2, enemiesCrashed, null, this);
 
 
+
   }
 
-  
+
 
   update() {
 
@@ -164,7 +202,7 @@ const enemies2 = this.physics.add.group({
 
     if (this.cursors.up.isDown) {
       this.player.moveUp();
-     
+
     } else if (this.cursors.down.isDown) {
       this.player.moveDown();
     }
@@ -175,21 +213,15 @@ const enemies2 = this.physics.add.group({
       this.player.moveRight();
     }
 
-    
+
   }
 }
 
+let score = 0;
+let scoreText;
+
+
 const enemiesCrashed = (player, enemy) => {
-    enemy.disableBody(true, true);
-    console.log('Game Over')
-}
-
-const goldCollected = (player, gold) => {
-    gold.disableBody(true, true);
-    console.log('gold collected')
-}
-
-const silverCollected = (player, silver) => {
-    silver.disableBody(true, true);
-    console.log('silver collected')
+  enemy.disableBody(true, true);
+  console.log('Game Over')
 }
