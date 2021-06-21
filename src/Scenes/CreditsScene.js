515949b691c4ac71f60563c321cbf17ce80e23c4
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-06-21 03:41:21 -0300

    Add developer information in Credits

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -8,7 +8,8 @@ export default class CreditsScene extends Phaser.Scene {
 
   create () {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Miguel Tucu Gomez', { fontSize: '26px', fill: '#fff' });
+    this.twitterText = this.add.text(0, 0, 'Twitter: @Qete_arg', { fontSize: '24px', fill: '#39a1f2' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
 
     Phaser.Display.Align.In.Center(
@@ -21,14 +22,20 @@ export default class CreditsScene extends Phaser.Scene {
       this.zone
     );
 
+    Phaser.Display.Align.In.Center(
+      this.twitterText,
+      this.zone
+    );
+
     this.madeByText.setY(1000);
+    this.twitterText.setY(1100);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
       y: -100,
       ease: 'Power1',
-      duration: 3000,
-      delay: 1000,
+      duration: 800,
+      delay: 2800,
       onComplete: function () {
         this.destroy;
       }
@@ -42,8 +49,20 @@ export default class CreditsScene extends Phaser.Scene {
       delay: 1000,
       onComplete: function () {
         this.madeByTween.destroy;
+      }.bind(this)
+    });
+
+    this.twitterTextTween = this.tweens.add({
+      targets: this.twitterText,
+      y: -400,
+      ease: 'Power1',
+      duration: 9000,
+      delay: 1000,
+      onComplete: function () {
+        this.twitterText.destroy;
         this.scene.start('Title');
       }.bind(this)
     });
+
   }
 };
