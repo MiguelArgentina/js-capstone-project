067d0e6cd518640d75d1cfb31252c8881f2c04b1
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-06-21 16:49:01 -0300

    Change directory path

--- webpack/prod.js
+++ webpack/prod.js
@@ -6,7 +6,8 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(base, {
   mode: "production",
   output: {
-    filename: "bundle.min.js"
+    filename: "bundle.min.js",
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: false,
   performance: {
